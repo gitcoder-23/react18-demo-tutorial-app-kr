ff87ae719a0ab4cd0881b7f955fbf8aeb74ab65f
prabhat-cn <prabhat@capitalnumbers.com>
2023-10-15 13:36:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoApp from '../src/demoApps/TodoApp';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Demo Application</h1>
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;

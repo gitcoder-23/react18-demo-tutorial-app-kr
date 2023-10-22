3cdc0426563acd8d841918eceb5ec63f65d2f3a5
Krittibas-27 <lets1954@gmail.com>
2023-10-22 12:11:36 +0530

    modify file update

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
+import {Navbar,Container,Nav } from 'react-bootstrap';
 
 const MenuBar = () => {
   return (
-    <DropdownButton id="dropdown-item-button" title="Tutorial Menu">
-      <Dropdown.Item href="/">Todo App</Dropdown.Item>
-      <Dropdown.Item href="/task/newtodo">NewTodo App</Dropdown.Item>
-    </DropdownButton>
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-right">
+            <Nav.Link href="/">Todo App</Nav.Link>
+            <Nav.Link href="/task/newtodo">NewTodo App</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
+    </>
+    
   );
 };
 
--- src/demoApps/NewTodoApp.js
+++ src/demoApps/NewTodoApp.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import './todo.css';
+import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 
 const NewTodoApp = () => {
   const [todoInput, setTodoInput] = useState('');
   const [listTodos, setListTodos] = useState([]);
   const [errorMsg, setErrorMsg] = useState('');
+  const [editItem, SetEditItem] = useState(true)
+  const [updateData, setUpdatedata] = useState(null)
 
   const addTodo = () => {
     if (todoInput === '' || !todoInput) {
@@ -22,64 +25,88 @@ const NewTodoApp = () => {
     }
   };
 
-  return (
-    <div className="todo_container">
-      <div className="input_head">
-        <form>
-          <input
-            style={{ fontSize: '20px' }}
-            type="text"
-            name="todoinput"
-            id="todoinput"
-            value={todoInput}
-            placeholder="Enter todo here"
-            onChange={(e) => setTodoInput(e.target.value)}
-          />
-          &nbsp;
-          <button
-            type="button"
-            style={{ fontSize: '20px', background: 'green', color: '#fff' }}
-            onClick={() => addTodo()}
-            disabled={!todoInput}
-          >
-            Add
-          </button>
-        </form>
-        <p style={{ color: 'red' }}>{errorMsg}</p>
-      </div>
+  const EditHandler = (e) => {
+    setUpdatedata(e)
+    SetEditItem(false)
+  }
 
-      {listTodos.length === 0 ? (
-        <h3>No todo available!</h3>
-      ) : (
-        <div className="main_table">
-          <table>
-            <thead>
-              <tr>
-                <th>#Sl. No</th>
-                <th>Todo Name</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            {listTodos.map((tdata, index) => {
-              console.log('tdata=>', tdata);
-              return (
-                <tbody key={tdata.id}>
-                  <tr>
-                    <td>{index + 1}</td>
-                    <td>{tdata.todo}</td>
-                    <td>
-                      <button> View</button>&nbsp;&nbsp;
-                      <button> Edit</button>&nbsp;&nbsp;
-                      <button> Delete</button>
-                    </td>
-                  </tr>
-                </tbody>
-              );
-            })}
-          </table>
-        </div>
-      )}
-    </div>
+  return (
+    <>
+      <Container className='text-center'>
+        <Row className="justify-content-md-center">
+          <Col xs={6}>
+            <Form inline >
+              <Row>
+                <Col xs={9}>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter todo here"
+                    name="todoinput"
+                    id="todoinput"
+                    value={todoInput}
+                    onChange={(e) => setTodoInput(e.target.value)}
+                  />
+                </Col>
+                <Col>
+                  <Button type="submit" onClick={() => addTodo()}
+                    disabled={!todoInput} className='w-100'>Submit</Button>
+
+                </Col>
+              </Row>
+              {
+                listTodos.length === 0 ?
+                  <h3>No todo available!</h3> :
+                  <table className='w-100 my-4'>
+                    <thead>
+                      <tr>
+                        <th>#Sl. No</th>
+                        <th>Todo Name</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    {listTodos.map((tdata, index) => {
+                      console.log('tdata=>', tdata);
+                      return (
+                        <tbody key={tdata.id}>
+                          <tr>
+                            <td>{index + 1}</td>
+                            <td>{tdata.todo}</td>
+                            {
+                              !editItem ?
+                              <td>
+                                <Button onClick={() => addTodo()}
+                                  className='w-100'>Edit Now</Button>
+                                  <Button variant="danger">Cancel</Button>
+                              </td>
+                                
+                                :
+                                <td>
+                                  <Button variant="primary">View</Button>
+                                  <Button onClick={() => EditHandler(tdata)} variant="success">Edit</Button>
+                                  <Button variant="danger">Delete</Button>
+                                </td>
+                            }
+
+                          </tr>
+                        </tbody>
+                      );
+                    })}
+                  </table>
+
+              }
+              <Row>
+                <Col>
+
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
+
+      </Container>
+
+    </>
+
   );
 };
 

be0526657286dbe93e533091e2dba8f01f61efb0
prabhat-cn <prabhat@capitalnumbers.com>
2023-10-29 19:16:19 +0530

    🎉

--- src/CrudUsingFakeJsonApi/UserList.js
+++ src/CrudUsingFakeJsonApi/UserList.js
@@ -11,6 +11,10 @@ const UserList = () => {
 
   const [showViewModal, setShowViewModal] = useState(false);
 
+  //Search
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
   const getAllUsers = () => {
     setIsLoading(true);
     // Async OPS
@@ -31,14 +35,11 @@ const UserList = () => {
       });
   };
 
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
-
   useEffect(() => {
     getAllUsers();
   }, []);
 
-  // console.log('userAllDatas-=>', userAllDatas);
+  console.log('userAllDatas-=>', userAllDatas);
 
   const viewUserDetails = (vdata) => {
     // console.log('vdata=>', vdata);
@@ -48,53 +49,95 @@ const UserList = () => {
     }
   };
 
-  console.log('viewUserData=>', viewUserData);
+  const searchSubmit = (evt) => {
+    evt.preventDefault();
+    setQuery(search);
+  };
+
+  const resetSearch = () => {
+    setSearch('');
+    setQuery('');
+  };
 
   return (
     <div className="container">
       <h2>Json API CRUD</h2>
+
       {isLoading ? (
         <SpinnerComponent />
       ) : isError ? (
         <h4 style={{ color: 'red' }}> Something went wrong!</h4>
       ) : (
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#Sl.No.</th>
-              <th>User Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          {userAllDatas &&
-            userAllDatas.map((udata) => {
-              return (
-                <tbody key={udata.id}>
-                  <tr>
-                    <td>{udata.id}</td>
-                    <td>{udata.name}</td>
-                    <td>{udata.email}</td>
-                    <td>{udata.phone}</td>
-                    <td>
-                      <Button
-                        className="mx-1"
-                        variant="success"
-                        onClick={() => viewUserDetails(udata)}
-                      >
-                        View
-                      </Button>&nbsp;
-                      <Button className="mx-1" variant="danger">
-                        Delete
-                      </Button>
-                    </td>
-                    
-                  </tr>
-                </tbody>
-              );
-            })}
-        </Table>
+        <>
+          <div className="row">
+            <div className="col-md-4">
+              <form onSubmit={() => searchSubmit()}>
+                <input
+                  type="text"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />{' '}
+                &nbsp;
+                <button type="button" onClick={() => resetSearch()}>
+                  Reset
+                </button>
+              </form>
+            </div>
+          </div>
+          <Table striped bordered hover variant="dark" className="mt-4">
+            <thead>
+              <tr>
+                <th>#Sl.No.</th>
+                <th>User Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            {userAllDatas &&
+              userAllDatas
+                .filter((val) => {
+                  // console.log('val=>', val);
+                  if (search === '') {
+                    return val;
+                  } else if (
+                    val.name.toLowerCase().includes(search.toLowerCase())
+                  ) {
+                    return val;
+                  } else if (
+                    val.email.toLowerCase().includes(search.toLowerCase())
+                  ) {
+                    return val;
+                  }
+                })
+
+                .map((udata) => {
+                  return (
+                    <tbody key={udata.id}>
+                      <tr>
+                        <td>{udata.id}</td>
+                        <td>{udata.name}</td>
+                        <td>{udata.email}</td>
+                        <td>{udata.phone}</td>
+                        <td>
+                          <Button
+                            className="mx-1"
+                            variant="success"
+                            onClick={() => viewUserDetails(udata)}
+                          >
+                            View
+                          </Button>
+                          &nbsp;
+                          <Button className="mx-1" variant="danger">
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+          </Table>
+        </>
       )}
 
       {/* View Modal Start */}

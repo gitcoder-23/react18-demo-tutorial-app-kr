8e48a6290e62fbcc0b79b929765e1c2f0e0928f5
Krittibas-27 <lets1954@gmail.com>
2023-10-29 20:49:45 +0530

    Complete

--- src/CrudUsingFakeJsonApi/UserList.js
+++ src/CrudUsingFakeJsonApi/UserList.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Table, Button, Modal } from 'react-bootstrap';
-import SpinnerComponent from '../components/SpinnerComponent';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Table, Button, Modal, Form, Row, Col } from "react-bootstrap";
+import SpinnerComponent from "../components/SpinnerComponent";
 
 const UserList = () => {
   const [userAllDatas, setUserAllDatas] = useState([]);
@@ -9,11 +9,17 @@ const UserList = () => {
   const [isError, setIsError] = useState(false);
   const [viewUserData, setViewUserData] = useState({});
 
+ 
+  //Search
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
   const [showViewModal, setShowViewModal] = useState(false);
 
-  //Search
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('');
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [editUserData, serEditUserData] = useState({})
+  const [err, setErr]=useState('')
+
 
   const getAllUsers = () => {
     setIsLoading(true);
@@ -29,7 +35,7 @@ const UserList = () => {
         }
       })
       .catch((err) => {
-        console.log('err=>', err);
+        console.log("err=>", err);
         setIsError(true);
         setIsLoading(false);
       });
@@ -39,7 +45,7 @@ const UserList = () => {
     getAllUsers();
   }, []);
 
-  console.log('userAllDatas-=>', userAllDatas);
+  //console.log("userAllDatas-=>", userAllDatas);
 
   const viewUserDetails = (vdata) => {
     // console.log('vdata=>', vdata);
@@ -55,9 +61,50 @@ const UserList = () => {
   };
 
   const resetSearch = () => {
-    setSearch('');
-    setQuery('');
+    setSearch("");
+    setQuery("");
   };
+
+  const deleteUser = (crr) => {
+    if (window.confirm("Do you want to delete?")) {
+      const updateAllUser = userAllDatas.filter((user) => {
+        return user.id !== crr.id;
+      });
+      setUserAllDatas(updateAllUser);
+    }
+  };
+
+  const editUser = (data) => {
+    setShowEditModal(true);
+    serEditUserData(data)
+  }
+
+  const cancelEdit=()=>{
+    setShowEditModal(false);
+    serEditUserData({})
+  }
+
+  const SubmitData=()=>{
+    if(editUserData=== ''){
+      setErr('Edit your name')
+    }else{
+      const updateAllUser = userAllDatas.map((item)=>{
+        if(item.id===editUserData.id){
+          return {
+            ...userAllDatas, 
+            name:editUserData.name, 
+            email:editUserData.email,
+            phone:editUserData.phone
+          }
+        }
+        return item
+      })
+      console.log('updateAllUser -', updateAllUser)
+      setUserAllDatas(updateAllUser)
+    }
+  }
+
+  //console.log('editUserData -', userAllDatas)
 
   return (
     <div className="container">
@@ -66,20 +113,32 @@ const UserList = () => {
       {isLoading ? (
         <SpinnerComponent />
       ) : isError ? (
-        <h4 style={{ color: 'red' }}> Something went wrong!</h4>
+        <h4 style={{ color: "red" }}> Something went wrong!</h4>
       ) : (
         <>
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-6 offset-md-3">
               <form onSubmit={() => searchSubmit()}>
                 <input
                   type="text"
                   value={search}
                   onChange={(e) => setSearch(e.target.value)}
-                />{' '}
+                />{" "}
                 &nbsp;
-                <button type="button" onClick={() => resetSearch()}>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => resetSearch()}
+                >
                   Reset
+                </button>{" "}
+                &nbsp;
+                <button
+                  type="button"
+                  className="btn btn-dark"
+                  onClick={() => resetSearch()}
+                >
+                  Add User
                 </button>
               </form>
             </div>
@@ -98,7 +157,7 @@ const UserList = () => {
               userAllDatas
                 .filter((val) => {
                   // console.log('val=>', val);
-                  if (search === '') {
+                  if (search === "") {
                     return val;
                   } else if (
                     val.name.toLowerCase().includes(search.toLowerCase())
@@ -111,11 +170,11 @@ const UserList = () => {
                   }
                 })
 
-                .map((udata) => {
+                .map((udata, index) => {
                   return (
                     <tbody key={udata.id}>
                       <tr>
-                        <td>{udata.id}</td>
+                        <td>{index + 1}</td>
                         <td>{udata.name}</td>
                         <td>{udata.email}</td>
                         <td>{udata.phone}</td>
@@ -128,7 +187,19 @@ const UserList = () => {
                             View
                           </Button>
                           &nbsp;
-                          <Button className="mx-1" variant="danger">
+                          <Button
+                            className="mx-1"
+                            variant="info"
+                            onClick={() => editUser(udata)}
+                          >
+                            Edit
+                          </Button>
+                          &nbsp;
+                          <Button
+                            className="mx-1"
+                            variant="danger"
+                            onClick={() => deleteUser(udata)}
+                          >
                             Delete
                           </Button>
                         </td>
@@ -153,6 +224,8 @@ const UserList = () => {
         <Modal.Body>
           <div className="container">
             <h2>{viewUserData.name}</h2>
+            <p>Email - {viewUserData.email}</p>
+            <p>Phone - {viewUserData.phone}</p>
           </div>
         </Modal.Body>
         <Modal.Footer>
@@ -162,6 +235,57 @@ const UserList = () => {
         </Modal.Footer>
       </Modal>
       {/* View Modal End */}
+
+      {/* Edit Modal Start */}
+
+      <Modal
+        show={showEditModal}
+        onHide={() => setShowEditModal(false)}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Edit User Detail</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="container">
+            <Form className="justify-content-center mt-4">
+              <Form.Control
+                size="md"
+                className="m-1"
+                type="text"
+                value={editUserData.name}
+                placeholder="Edit your name here"
+                onChange={(e)=>serEditUserData(e.target.value)}
+              />
+
+              <Form.Control
+                size="md"
+                className="m-1"
+                type="text"
+                value={editUserData.email}
+                placeholder="Edit your email here"
+                onChange={(e)=>serEditUserData(e.target.value)}
+              />
+              <Form.Control
+                size="md"
+                className="m-1"
+                type="text"
+                value={editUserData.phone}
+                placeholder="Edit your phone here"
+                onChange={(e)=>serEditUserData(e.target.value)}
+              />
+              <Button type='button' className='m-3' variant="primary"  onClick={()=>SubmitData()}>Edit Submit</Button>
+              <Button type='button' className='m-3' variant="danger" onClick={()=>cancelEdit()}>Cancel</Button>
+            </Form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowEditModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {/* Edit Modal End */}
     </div>
   );
 };

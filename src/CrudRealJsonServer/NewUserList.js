ee186e7d7b1d8a22bfbd61d231d9099218735309
prabhat-cn <prabhat@capitalnumbers.com>
2023-11-04 13:39:48 +0530

    new json server api added

--- src/CrudRealJsonServer/NewUserList.js
+++ src/CrudRealJsonServer/NewUserList.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 
 const NewUserList = () => {
-  return <div>NewUserList</div>;
+  const [allUSerDatas, setAllUSerDatas] = useState([]);
+  const getAllUsers = () => {
+    axios
+      .get(`${process.env.REACT_APP_JSON_SERVER_BASE_URL}/user`)
+      .then((resp) => {
+        console.log('resp=>', resp);
+        setAllUSerDatas(resp.data.reverse());
+      })
+      .catch((err) => {
+        console.log('err=>', err);
+      });
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <h2>CRUD Using Json Sever</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>User Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        {allUSerDatas &&
+          allUSerDatas.map((udata, index) => {
+            return (
+              <tbody key={udata.id}>
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{udata.employeename}</td>
+                  <td>{udata.email}</td>
+                  <td>{udata.phone}</td>
+                </tr>
+              </tbody>
+            );
+          })}
+      </Table>
+    </div>
+  );
 };
 
 export default NewUserList;

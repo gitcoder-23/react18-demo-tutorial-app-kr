34ea719d309a35e343d00254814c943d73d79419
prabhat-cn <prabhat@capitalnumbers.com>
2023-11-05 13:06:16 +0530

    useLocation done

--- src/CrudRealJsonServer/ViewUserDetail.js
+++ src/CrudRealJsonServer/ViewUserDetail.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 const ViewUserDetail = () => {
   const navigate = useNavigate();
+  const { state } = useLocation();
+  console.log('state=>', state);
   const { vid } = useParams();
-  console.log('vid=>', vid);
+  // console.log('vid=>', vid);
 
   const [viewSngleUser, setViewSngleUser] = useState(null);
 
@@ -31,25 +33,47 @@ const ViewUserDetail = () => {
   }, []);
 
   return (
-    <Card>
-      <Card.Header as="h5">User Detail</Card.Header>
-      <Card.Body>
-        <Card.Title>{viewSngleUser?.employeename}</Card.Title>
-        <Card.Text>
-          <div>
-            <h4>Email: {viewSngleUser?.email}</h4>
-            <h4>Email: {viewSngleUser?.gender}</h4>
-            <h4>Email: {viewSngleUser?.phone}</h4>
-          </div>
-        </Card.Text>
-        <Button
-          variant="primary"
-          onClick={() => navigate('/jsonserver/newuserlist')}
-        >
-          Go Back
-        </Button>
-      </Card.Body>
-    </Card>
+    <>
+      <Card>
+        <Card.Header as="h5">User Detail Using useParams</Card.Header>
+        <Card.Body>
+          <Card.Title>{viewSngleUser?.employeename}</Card.Title>
+          <Card.Text>
+            <div>
+              <h4>Email: {viewSngleUser?.email}</h4>
+              <h4>Email: {viewSngleUser?.gender}</h4>
+              <h4>Email: {viewSngleUser?.phone}</h4>
+            </div>
+          </Card.Text>
+          <Button
+            variant="primary"
+            onClick={() => navigate('/jsonserver/newuserlist')}
+          >
+            Go Back
+          </Button>
+        </Card.Body>
+      </Card>
+
+      <Card>
+        <Card.Header as="h5">User Detail Using useLocation</Card.Header>
+        <Card.Body>
+          <Card.Title>{state.singleUser.employeename}</Card.Title>
+          <Card.Text>
+            <div>
+              <h4>Email: {state.singleUser.email}</h4>
+              <h4>Email: {state.singleUser.gender}</h4>
+              <h4>Email: {state.singleUser.phone}</h4>
+            </div>
+          </Card.Text>
+          <Button
+            variant="primary"
+            onClick={() => navigate('/jsonserver/newuserlist')}
+          >
+            Go Back
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 

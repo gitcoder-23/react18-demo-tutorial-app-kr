34cd8a5d6001d75ea46c1cf943eab967e51b5afb
prabhat-cn <prabhat@capitalnumbers.com>
2023-11-26 12:52:46 +0530

    route seperated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
-import TodoApp from "../src/demoApps/TodoApp";
-import NewTodoApp from "./demoApps/NewTodoApp";
-import { Route, Routes } from "react-router-dom";
 import MenuBar from "./components/MenuBar";
-import TaskTodoApp from "./demoApps/TaskTodoApp";
-import UserList from "./CrudUsingFakeJsonApi/UserList";
-import NewUserList from "./CrudRealJsonServer/NewUserList";
-import ViewUserDetail from "./CrudRealJsonServer/ViewUserDetail";
-import AddNewUser from "./CrudRealJsonServer/AddNewUser";
 import { ToastMessageComponent } from "./components/ToastMessageComponent";
-import EditNewUser from "./CrudRealJsonServer/EditNewUser";
-import RejexUserValidation from "./regexvalidation/RejexUserValidation";
+import ComponentRoute from "./ComponentRoute";
 
 function App() {
   return (
@@ -18,27 +9,7 @@ function App() {
       <ToastMessageComponent />
       <MenuBar />
       <h1>React Demo Application</h1>
-      <Routes>
-        <Route exact path="/" element={<TodoApp />} />
-        <Route exact path="/taskapp" element={<TaskTodoApp />} />
-        <Route exact path="/task/newtodo" element={<NewTodoApp />} />
-        <Route exact path="/json/usercrud" element={<UserList />} />
-
-        {/* CRUD App Using JSON Server */}
-        <Route exact path="/jsonserver/newuserlist" element={<NewUserList />} />
-        <Route exact path="/jsonserver/add/newuser" element={<AddNewUser />} />
-        <Route
-          exact
-          path="/jsonserver/edituser/:eid"
-          element={<EditNewUser />}
-        />
-        <Route
-          exact
-          path="/jsonserver/viewuser/:vid"
-          element={<ViewUserDetail />}
-        />
-        <Route exact path="/validation/regexvalidation" element={<RejexUserValidation/>} />
-      </Routes>
+      <ComponentRoute />
     </div>
   );
 }

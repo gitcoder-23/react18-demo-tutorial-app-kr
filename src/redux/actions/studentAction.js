9b53173ef679de1d5cba210b6a511142466007bb
Krittibas-27 <lets1954@gmail.com>
2023-12-17 12:59:49 +0530

    Reverse Section Done

--- src/redux/actions/studentAction.js
+++ src/redux/actions/studentAction.js
@@ -3,5 +3,5 @@ import { RootApi } from "../../RootApi";
 
 export const getAllStudent = createAsyncThunk("student/get", async()=>{
    const res = await RootApi.get("/student")
-   return res.data
+   return res.data.reverse()
 })
